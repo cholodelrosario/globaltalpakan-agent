e84644ad22e206e29fd46e30ab1bb8da1b2e1124
Cholz Del Rosario <chomigz12@gmail.com>
2021-04-09 05:01:09 +0800

    last transaction record

--- src/store/wallet/actions.js
+++ src/store/wallet/actions.js
@@ -28,15 +28,18 @@ export async function creditWallet ({rootState},payload) {
             await addCredits(payload.credits,payload.downlineID)
             .then(async ()=>{
                 await recordHistory(payload.to,account,payload.credits)
-                Dialog.create({
-                    title: `Sending Credits Successful !`,
-                    message: `${payload.credits} CREDITS to ${payload.to.accountPhone} - ${payload.to.accountName}`,
-                    type: 'info',
-                    color: 'primary',
-                    textColor: 'white',
-                    icon: 'info',
-                    ok: 'close',
-                    dark: true,
+                .then(async ()=>{
+                    await updateLastTransaction(payload.downlineID)
+                    Dialog.create({
+                        title: `Sending Credits Successful !`,
+                        message: `${payload.credits} CREDITS to ${payload.to.accountPhone} - ${payload.to.accountName}`,
+                        type: 'info',
+                        color: 'primary',
+                        textColor: 'white',
+                        icon: 'info',
+                        ok: 'close',
+                        dark: true,
+                    })
                 })
             })
         })
@@ -70,10 +73,30 @@ async function addCredits(credits,downlineID){
 
 async function recordHistory(to,from,amount){
     try {
-        const response = await firebaseDb.collection('CreditHistory').add({to:to,from:from,amount:amount,timestamp: new Date()})
+        let reciever = {...to}
+        let recieverID = reciever['.key']
+        delete reciever['.key']
+        reciever.accountID = recieverID
+
+        let sender = {...from}
+        let senderID = sender['.key']
+        delete sender['.key']
+        sender.accountID = senderID
+
+        const response = await firebaseDb.collection('CreditHistory').add({to:reciever,from:sender,amount:amount,timestamp: new Date()})
         if(response) { console.log('%c SUCCESS_CREDITS_HISTORY','background: #222; color: #bada55') }
     } catch (error) {
         console.log(error,'error')
         console.log('%c ERROR_CREDITS_HISTORY','background: #D50000; color: #fff')
+    }      
+}
+
+async function updateLastTransaction(downlineID){
+    try {
+        const response = await firebaseDb.collection('Players').doc(downlineID).update({ lastTransaction: new Date() })
+        if(response) { console.log('%c SUCCESS_LAST_TRANSACTION','background: #222; color: #bada55') }
+    } catch (error) {
+        console.log(error,'error')
+        console.log('%c ERROR_LAST_TRANSACTION','background: #D50000; color: #fff')
     }      
 }
